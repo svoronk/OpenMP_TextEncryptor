79fd4574b02a097efee2b3ad5a89547a2635e61a
Simonas Voronkevičius <simvor@ktu.lt>
2022-10-20 06:14:35 +0300

    Add project files.

---
+++ ThreadingB.cpp
@@ -0,0 +1,162 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <omp.h>
+#include <regex>
+using namespace std;
+
+struct SelectedDataStructure {
+    string StringProperty;
+    int LinePosition;
+};
+struct SelectedDataStructureWithComputedValue {
+    string DecryptedText;
+    string EncryptedText;
+    int LinePosition;
+};
+
+void Reader();
+string EncryptOneDataPoint(string string_line, bool EncryptedOne);
+string ConvertToString(char* a, int size);
+void Encrypt(vector<SelectedDataStructure> data);
+bool containsOnlyLetters(std::string const& str);
+
+bool SortByNumber(SelectedDataStructureWithComputedValue& a, SelectedDataStructureWithComputedValue& b) {
+    return a.LinePosition < b.LinePosition;
+}
+
+vector<SelectedDataStructure> selectedDataList;
+vector<SelectedDataStructureWithComputedValue> selectedResultList;
+int WorkerCount;
+
+
+int main()
+{
+    //1. Nuskaito duomenų failą į lokalų masyvą, sąrašą ar kitą duomenų struktūrą;
+    Reader();
+
+    //2. Padaliname vieną sarašą į daug atitinkamo dydžio sąrašų gijoms apdoroti
+    WorkerCount = (selectedDataList.size() / 4) < 2 ? 2 : selectedDataList.size() / 4;
+    //cout << selectedDataList.size() << " " << WorkerCount << endl;
+    
+    int subpart_of_vector = selectedDataList.size() / WorkerCount;
+    int module_of_vector = selectedDataList.size() % WorkerCount;
+    //cout << "division: " << subpart_of_vector << endl;
+    //cout << "module: " << module_of_vector << endl;
+
+
+    int counter = 0;
+    vector<vector<SelectedDataStructure>> subvectors(WorkerCount);
+    for (vector<SelectedDataStructure>& sub_vec : subvectors)
+    {
+        for (auto i = 0; i < subpart_of_vector; i++)
+        {
+            sub_vec.push_back(selectedDataList.at(counter));
+            counter++;
+        }
+        if (module_of_vector > 0)
+        {
+            sub_vec.push_back(selectedDataList.at(counter));
+            counter++;
+            module_of_vector--;
+        }
+    }
+
+    //3. Paleidžia pasirinktą kiekį darbininkių gijų 2 ≤ x ≤ n/4 (n — duomenų kiekis faile).
+    omp_set_num_threads(WorkerCount);
+#pragma omp parallel
+    {
+        vector<SelectedDataStructure> sub_vec;
+#pragma omp critical
+        {
+            sub_vec = subvectors.front();
+            subvectors.erase(subvectors.begin());
+        }
+        Encrypt(sub_vec);
+    }
+    //4. Iš rezultatų monitoriaus, kuriame saugomi gauti atfiltruoti rezultatai, rezultatus išveda į tekstinį failą lentele.
+}
+
+void Reader()
+{
+    string myText;
+    ifstream MyReadFile("Input.txt");
+    int count = 0;
+    while (getline(MyReadFile, myText)) {
+        SelectedDataStructure data;
+        data.StringProperty = myText;
+        data.LinePosition = count;
+        selectedDataList.push_back(data);
+        count++;
+    }
+    MyReadFile.close();
+}
+
+void Encrypt(vector<SelectedDataStructure> data)
+{
+    for (SelectedDataStructure item : data)
+    {
+        string EncText = EncryptOneDataPoint(item.StringProperty, true);
+        string DecText = EncryptOneDataPoint(item.StringProperty, false);
+
+        SelectedDataStructureWithComputedValue value;
+        value.DecryptedText = DecText;
+        value.EncryptedText = EncText;
+        value.LinePosition = item.LinePosition;
+        if (containsOnlyLetters(DecText))
+        {
+#pragma omp critical
+            {
+                selectedResultList.push_back(value);
+                std::sort(selectedResultList.begin(), selectedResultList.end(), SortByNumber);
+            }
+        }
+    }
+}
+
+string EncryptOneDataPoint(string string_line, bool EncryptedOne)
+{
+    //string string_line = "ashard";
+    int count = string_line.size();
+    int i;
+    char str[100];
+    char enc[100];
+    char dec[100];
+
+    for (auto i = 0; i < string_line.size(); i++)
+    {
+        str[i] = string_line[i];
+    }
+
+    for (i = 0; (i < 100 && str[i] != '\0'); i++)
+        enc[i] = str[i] + 2; //the key for encryption is 3 that is added to ASCII value
+
+    //cout << "\nEncrypted string: " << str << endl;
+
+    for (i = 0; (i < 100 && str[i] != '\0'); i++)
+        dec[i] = enc[i] - 2; //the key for encryption is 3 that is subtracted to ASCII value
+
+    //cout << "\nDecrypted string: " << str << endl;
+
+    string Encrypted = ConvertToString(enc, count);
+    string Decrypted = ConvertToString(dec, count);
+    return EncryptedOne == true ? Encrypted : Decrypted;
+}
+
+string ConvertToString(char* a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
+bool containsOnlyLetters(std::string const& str) {
+    return str.find_first_not_of(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") ==
+        std::string::npos;
+}
